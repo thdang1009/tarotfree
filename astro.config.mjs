22d5b3d2ebfb8cc7f9a57c1edf56cf09350322fc
Ghost <thdang1009@gmail.com>
2025-10-10 13:34:30 +0700

    fix: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://tarotfree.netlify.app',
-  output: 'static',
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   },
   build: {
-    format: 'directory', // each page in its own folder
+    format: 'directory'
   },
   integrations: [react(), sitemap()]
 });
